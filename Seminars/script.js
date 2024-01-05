b20655000e6f081e911c0a3ffe1f6f6005f59551
denisalja <dn746393@dal.ca>
2024-01-05 14:12:24 -0400

    added 2 seminars

--- Seminars/script.js
+++
@@ -1,7 +0,0 @@
-function toggleDate(dateId) {
-    const allDateContents = document.querySelectorAll('.date-content');
-    allDateContents.forEach(content => content.classList.remove('active'));
-
-    const selectedDateContent = document.getElementById('date-' + dateId);
-    selectedDateContent.classList.add('active');
-}
